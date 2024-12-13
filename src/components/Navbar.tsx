d3cb17382ecc866fee1238c271f1995f57c526da
MaiSang <sarusasiri@gmail.com>
2024-12-13 10:44:59 +0700

    animation navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,33 +45,32 @@ function Navbar() {
             onClick={toggleMenu}
           />
         </div>
-        {isMenuOpen && (
-          <nav className="absolute top-20 right-0 left-0 bg-black p-5  z-10 ">
-            <ul className="flex flex-col space-y-4">
-              <li>
-                <Link to="/" onClick={toggleMenu}>
-                  Explore
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/tarotdeck"
-                  onClick={() => {
-                    tarotContext?.fetchCards();
-                    toggleMenu();
-                  }}
-                >
-                  Tarot Deck
-                </Link>
-              </li>
-              <li>
-                <Link to="/tarotdraw" onClick={toggleMenu}>
-                  Your Tarot Draw
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        )}
+        <nav
+          className={`transition-all duration-[.7s] absolute top-20 right-0 left-0 bg-black p-5 z-10 block ${
+            isMenuOpen
+              ? "opacity-100 translate-x-0"
+              : "opacity-0 translate-x-full"
+          } `}
+        >
+          <ul className="flex flex-col space-y-4">
+            <li className="hover:underline">
+              <Link to="/">Explore</Link>
+            </li>
+            <li className="hover:underline">
+              <Link
+                to="/tarotdeck"
+                onClick={() => {
+                  tarotContext?.fetchCards();
+                }}
+              >
+                Tarot Deck
+              </Link>
+            </li>
+            <li className="hover:underline">
+              <Link to="/tarotdraw">Your Tarot Draw</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
